8f3cba175e449bcef4acca90585fdc31c7162b06
poipoi0099 <56574097+poipoi0099@users.noreply.github.com>
2020-04-04 23:14:17 +0700

    Tag

--- src/Tag/App.js
+++ src/Tag/App.js
@@ -1,1 +1,72 @@
-a
+import React, { Component } from 'react';
+import TagListItem from "./TagList.js"
+import SubmitForm from "./TagForm.js"
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      taglist : [
+        {
+          tag: "#comedy",
+          cur: false,
+          color: ' black'
+        },
+        {
+          tag: "#la",
+          cur: false,
+          color: 'black'
+        }
+      ]
+    }
+    this.MarkCurrent = this.MarkCurrent.bind(this);
+    this.handleAddTag = this.handleAddTag.bind(this);
+  }
+
+  handleAddTag = (item) => {
+    this.state.taglist.push(item);
+    this.setState({ taglist: this.state.taglist });
+  };
+
+  MarkCurrent = index => {
+    var select = this.state.taglist[index]
+    this.state.taglist.splice(index, 1);
+    select.cur = !select.cur; 
+    select.cur ? this.state.taglist.push(select) : this.state.taglist.unshift(select);
+    this.setState({ taglist: this.state.taglist });
+
+  };
+  
+
+  render() {
+    let items = this.state.taglist.map((item, index) => {
+      let result = ""
+      if (item) {
+        result = <TagListItem
+                  key={index}
+                  tag={item.tag}
+                  cur={item.cur}
+                  onClick={() => this.MarkCurrent(index)}
+                />
+      }
+
+      return result
+    })
+    return (
+      <div className="App">
+        <div className="App-form">
+          <SubmitForm
+           onAddTag={this.handleAddTag}
+
+          />
+        </div>
+        <div>
+          {items}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
