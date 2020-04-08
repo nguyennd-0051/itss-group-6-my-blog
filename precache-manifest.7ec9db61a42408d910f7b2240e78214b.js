a313114ab7c3e9d70aa6577aff3da013da18521a
nguyennd-0051 <nguyen.duy.nguyen@sun-asterisk.com>
2020-04-08 23:40:56 +0700

    Updates

---
+++ precache-manifest.7ec9db61a42408d910f7b2240e78214b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0b6a6306aee59141e8761e2517e277",
+    "url": "/itss_group6_blog/index.html"
+  },
+  {
+    "revision": "e2d64808a0431a48672d",
+    "url": "/itss_group6_blog/static/css/2.297af936.chunk.css"
+  },
+  {
+    "revision": "105a1ac57a80da8c74d0",
+    "url": "/itss_group6_blog/static/css/main.34e2756c.chunk.css"
+  },
+  {
+    "revision": "e2d64808a0431a48672d",
+    "url": "/itss_group6_blog/static/js/2.71f6529c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/itss_group6_blog/static/js/2.71f6529c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "105a1ac57a80da8c74d0",
+    "url": "/itss_group6_blog/static/js/main.2081451e.chunk.js"
+  },
+  {
+    "revision": "b9ab45c2a2039da0c2e9",
+    "url": "/itss_group6_blog/static/js/runtime-main.70735d75.js"
+  }
+]);
